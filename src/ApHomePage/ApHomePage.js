fa29b783b06d8ecae88b2177dc17557d74d2b23e
Lucas Fasolato <fasolatolucas14@gmail.com>
2022-09-08 16:21:25 -0300

    generate random numbers and detect user input

--- src/ApHomePage/ApHomePage.js
+++ src/ApHomePage/ApHomePage.js
@@ -1,41 +1,59 @@
-import {React, useState} from 'react'
+import {React, useState, useEffect} from 'react'
 import './ApHomePage.css';
 
 function ApHomePage() {
     
     const [validateB, setValidateB] = useState (true);
     const [inputNumber, setInputNumber] = useState("");
-    const [number1, setNumber1] = useState();
-    const [number2, setNumber2] = useState();
+    const [number1, setNumber1] = useState(1);
+    const [number2, setNumber2] = useState(1);
 
     function validateNumber () {
-        if (parseInt(inputNumber) <= 12) {
+        if(inputNumber === "") {
+            setValidateB(true);
+        } else if(parseInt(inputNumber) <= 12) {
             setValidateB(false);
         } else {
             setValidateB(true);
         }
     }
 
-    function generateNumbers () {
-        setNumber1(Math.random* (12 - 1 + 1) + 1);
-        setNumber2(Math.random* (12 - 1 + 1) + 1);
-        document.getElementById('n1').innerHTML( String(number1));
-        document.getElementById('n2').innerHTML( String(number2));
+    function generateNumbers() {
+        setNumber1(getRandomNumber(1,12));
+        setNumber2(getRandomNumber(1,12));
     }
 
+    function getRandomNumber(min, max) {
+        return Math.trunc(Math.random() * (max - min) + min) ;
+      }
+
+      useEffect(() => {
+        console.log(inputNumber);
+        validateNumber(inputNumber);
+      }, [inputNumber]);
 
   return (
     <div className='home_size'>
-        <div>
+        <div className='content_box'>
             <h1>Juego de apuestas</h1>
-            <input id='inputNumber' onChange={(event) => {setInputNumber(event.target.value); validateNumber(); console.log(inputNumber)} } placeholder='Ingrese un numero del 1 al 12'/>
+            <section className='generation_apuesta'>
+                {/* onChange={(event) => {setInputNumber(event.target.value); validateNumber(); console.log(inputNumber)} } */}
+                <input className='input_box' id='inputNumber' onChange={(event) => setInputNumber(event.target.value)}  placeholder='Ingrese un numero del 1 al 12'/>
+                <button className='button_box' disabled={validateB} onClick={()=> generateNumbers()}>Jugar</button>
+            </section>
+            
+            <section className='result_apuesta'>
+                <h2>Su apuesta es: {inputNumber}</h2>
+                <div className='randoms_numbers'>
+                    <h2>Los numeros obtenidos en los dados fueron:</h2>
+                    <div className='results'>
+                    <p id='n1'>{number1}</p>
+                    <p id='n2'>{number2}</p>
+                    </div>
+                </div>
+            </section>
+            
         </div>
-      <button id='button_jugar' disabled={validateB} onClick={()=> generateNumbers()}>Jugar</button>
-      <div className='randoms_numbers'>
-        <h2>Los numeros obtenidos en los dados fueron:</h2>
-        <p id='n1'></p>
-        <p id='n2'></p>
-      </div>
     </div>
     
 
